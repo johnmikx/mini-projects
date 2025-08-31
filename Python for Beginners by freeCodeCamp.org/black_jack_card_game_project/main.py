6c3348b923a789c1b4273f5b89a02e9016d29136
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-08-31 18:19:24 +0800

    feat: blackjack deck class

--- Python for Beginners by freeCodeCamp.org/black_jack_card_game_project/main.py
+++ Python for Beginners by freeCodeCamp.org/black_jack_card_game_project/main.py
@@ -1,37 +1,43 @@
 import random
 
-cards = []
-suits = ['spades', 'clubs', 'hearts', 'diamonds']
-ranks = [
-    {'rank': 'A', 'value': 11},
-    {'rank': '2', 'value': 2},
-    {'rank': '3', 'value': 3},
-    {'rank': '4', 'value': 4},
-    {'rank': '5', 'value': 5},
-    {'rank': '6', 'value': 6},
-    {'rank': '7', 'value': 7},
-    {'rank': '8', 'value': 8},
-    {'rank': '9', 'value': 9},
-    {'rank': '10', 'value': 10},
-    {'rank': 'J', 'value': 10},
-    {'rank': 'Q', 'value': 10},
-    {'rank': 'K', 'value': 10}
-]
+class Deck():
+    def __init__(self):
+        self.cards = []
+        suits = ['spades', 'clubs', 'hearts', 'diamonds']
+        ranks = [
+            {'rank': 'A', 'value': 11},
+            {'rank': '2', 'value': 2},
+            {'rank': '3', 'value': 3},
+            {'rank': '4', 'value': 4},
+            {'rank': '5', 'value': 5},
+            {'rank': '6', 'value': 6},
+            {'rank': '7', 'value': 7},
+            {'rank': '8', 'value': 8},
+            {'rank': '9', 'value': 9},
+            {'rank': '10', 'value': 10},
+            {'rank': 'J', 'value': 10},
+            {'rank': 'Q', 'value': 10},
+            {'rank': 'K', 'value': 10}
+        ]
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
 
-def shuffle():
-    random.shuffle(cards)
+    def shuffle(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)
 
-def deal(number):
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
-
-shuffle()
-card = deal(1)[0]
-print(card[1]['value'])
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt
+    
+deck1 = Deck()
+deck2 = Deck()
+deck2.shuffle()
+print(deck1.cards)
+print(deck2.cards)
